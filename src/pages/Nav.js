50cb4fdd55d7255448943c7b3a43dcb9a7f49a70
Muhammad Usman <mrmuhammadusman1@gmail.com>
2021-10-07 12:08:11 +0500

    Routing in ReactJS

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,24 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-const Nav = () => {
+const Nav = (props) => {
     const navStyle = {
         color: 'white',
         linkStyle: 'none'
+    }
+
+    const aboutUs = () => {
+        props.history.push('/about')
+    }
+    const contactUs = () => {
+        props.history.push('/contact')
     }
     return (
         <div className='nav'>
             <Link style={navStyle} to="/"><h1>Logo</h1></Link>
             <ul className="nav-links">
-                <Link style={navStyle} to='/about'>
-                    <li>About Us</li>
-                </Link>
-                <Link style={navStyle} to='/contact'>
-                    <li>Contact Us</li>
-                </Link>
+                <li>
+                    <button className='btn' onClick={aboutUs}>About Us</button>
+                </li>
+                <li>
+                    <button className='btn' onClick={contactUs}>Contact Us</button>
+                </li>
             </ul>
 
         </div>
     )
 }
 
-export default Nav
+export default withRouter(Nav);
